71e7861d67ad3a80f18900afefffd0721227aec1
xsilvermc <57896691+xsilvermc@users.noreply.github.com>
2023-06-06 11:06:47 +0200

    project setup

---
+++ scripts/content.js
@@ -0,0 +1,8 @@
+/**
+ * TODO:
+ *  - more extensive lists
+ *  - load lists from file
+ *  - add options page to select lists to use and add/remove own keywords
+ *  - scan web page for keywords
+ *  - generate element to block sections with keywords (w/ keyword/category display and option to unblock)
+ */
