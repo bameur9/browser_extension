e0a08e58a1d93207285fcfbda848e2895b1c3d1b
mbameur <sdongmo@stud.hs-bremen.de>
2023-07-04 17:24:55 +0200

    first commit

---
+++ background.js
@@ -0,0 +1,18 @@
+// background.js
+
+// Liste von verbotenen Begriffen
+var blockedTerms = ["sexuell", "gewalttätig"];
+
+// Ereignishandler für Web-Anfragen
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+        for (var i = 0; i < blockedTerms.length; i++) {
+            var term = blockedTerms[i];
+            var regex = new RegExp("\\b" + term + "\\b", "gi");
+            if (details.url.match(regex)) {
+                return { cancel: true };
+            }
+        }
+        return { cancel: false };
+    }, { urls: ["<all_urls>"] }, ["blocking"]
+);
---
+++ content.js
@@ -0,0 +1,32 @@
+// content.js
+
+// Liste von verbotenen Begriffen
+// var blockedTerms = ["sexuell", "gewalttätig", "Selbstmord"];
+
+// Funktion zum Ersetzen der blockierten Begriffe
+function replaceBlockedTerms(node) {
+    var childNodes = node.childNodes;
+    for (var i = 0; i < childNodes.length; i++) {
+        var childNode = childNodes[i];
+        if (childNode.nodeType === Node.TEXT_NODE) {
+            var content = childNode.textContent;
+            console.log()
+            for (var j = 0; j < blockedTerms.length; j++) {
+                var term = blockedTerms[j];
+                var regex = new RegExp("\\b" + term + "\\b", "gi");
+                content = content.replace(regex, "****"); // Ersetze durch den Block
+            }
+            childNode.textContent = content;
+        } else {
+            replaceBlockedTerms(childNode); // Rekursiver Aufruf für verschachtelte Knoten
+        }
+    }
+}
+
+// Hauptfunktion zum Blockieren von Inhalten
+function blockContent() {
+    replaceBlockedTerms(document.body);
+}
+
+// Rufe die Hauptfunktion auf, wenn die Seite geladen wurde
+blockContent();
