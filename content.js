ddc78da0ebc978820fe40fcab04b966919b5ba2c
mbameur <sdongmo@stud.hs-bremen.de>
2023-07-04 21:16:57 +0200

    version_3

--- content.js
+++ content.js
@@ -1,9 +1,9 @@
 // Liste des mots indésirables
-const motsIndesirables = ["sexuell", "gewalttätig"];
+const motsIndesirables = ["sexuell", "Selbstmord"];
 
 // Fonction pour masquer le contenu
 function masquerContenu() {
-    const paragraphes = document.querySelectorAll("p, h1, h2, h3, h4, h5, h6, li, td, th");
+    const paragraphes = document.querySelectorAll("p, h1, h2, h3, h4, h5, h6, li, td, th, cite, span, em");
 
     for (let i = 0; i < paragraphes.length; i++) {
         const paragraphe = paragraphes[i];
@@ -15,11 +15,23 @@ function masquerContenu() {
             if (texte.includes(motIndesirable)) {
                 const bloc = document.createElement("div");
                 bloc.classList.add("texte-bloc");
-                bloc.innerText = "Cliquez pour afficher le contenu";
+                bloc.innerText = "Klicken Sie, um den Inhalt zu sehen";
+
+                bloc.style.padding = "10px";
+                bloc.style.margin = "30px";
+                bloc.style.width = "100%";
+                bloc.style.minHeight = "100px";
+                bloc.style.borderColor = "red";
+                bloc.style.borderWidth = "thick"
+                bloc.style.background = "#ccc";
+                bloc.style.color = "#000";
+                bloc.style.cursor = "pointer";
+
 
                 bloc.addEventListener("click", function() {
                     paragraphe.style.display = "block";
                     bloc.style.display = "none";
+
                 });
 
                 paragraphe.style.display = "none";
