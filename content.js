b76d179c0212ac0ddc6270d2c83ae37ce72e82fe
mbameur <sdongmo@stud.hs-bremen.de>
2023-07-16 14:42:25 +0200

    In this version, a block has been added. When you click on it, it is removed.  */

--- content.js
+++ content.js
@@ -1,46 +1,49 @@
-// Liste des mots indésirables
-const motsIndesirables = ["sexuell", "Selbstmord"];
+/**
+ * In this version, a block has been added. When you click on it, it is removed.
+ */
 
-// Fonction pour masquer le contenu
-function masquerContenu() {
-    const paragraphes = document.querySelectorAll("p, h1, h2, h3, h4, h5, h6, li, td, th, cite, span, em");
+// List of undesirable words
+const unwantedWords = ["violence", "suicide", "cocaine"];
 
-    for (let i = 0; i < paragraphes.length; i++) {
-        const paragraphe = paragraphes[i];
-        const texte = paragraphe.innerText.toLowerCase();
+// Function to hide content
+function hideContent() {
+    const paragraphs = document.querySelectorAll("p, h1, h2, h3, h4, h5, h6, li, td, th, cite, span, em");
 
-        for (let j = 0; j < motsIndesirables.length; j++) {
-            const motIndesirable = motsIndesirables[j];
+    for (let i = 0; i < paragraphs.length; i++) {
+        const paragraph = paragraphs[i];
+        const text = paragraph.innerText.toLowerCase();
 
-            if (texte.includes(motIndesirable)) {
-                const bloc = document.createElement("div");
-                bloc.classList.add("texte-bloc");
-                bloc.innerText = "Klicken Sie, um den Inhalt zu sehen";
+        for (let j = 0; j < unwantedWords.length; j++) {
+            const unwantedWord = unwantedWords[j];
 
-                bloc.style.padding = "10px";
-                bloc.style.margin = "30px";
-                bloc.style.width = "100%";
-                bloc.style.minHeight = "100px";
-                bloc.style.borderColor = "red";
-                bloc.style.borderWidth = "thick"
-                bloc.style.background = "#ccc";
-                bloc.style.color = "#000";
-                bloc.style.cursor = "pointer";
+            if (text.includes(unwantedWord)) {
+                const block = document.createElement("div");
+                block.classList.add("text-block");
+                block.innerText = "Click to reveal content";
 
+                block.style.padding = "10px";
+                block.style.margin = "30px";
+                block.style.width = "100%";
+                block.style.minHeight = "100px";
+                block.style.borderColor = "red";
+                block.style.borderWidth = "thick";
+                block.style.background = "#ccc";
+                block.style.color = "#000";
+                block.style.cursor = "pointer";
 
-                bloc.addEventListener("click", function() {
-                    paragraphe.style.display = "block";
-                    bloc.style.display = "none";
-
+                // Klicken Sie, um den Inhalt zu sehen
+                block.addEventListener("click", function() {
+                    paragraph.style.display = "block";
+                    block.style.display = "none";
                 });
 
-                paragraphe.style.display = "none";
-                paragraphe.parentNode.insertBefore(bloc, paragraphe);
+                paragraph.style.display = "none";
+                paragraph.parentNode.insertBefore(block, paragraph);
                 break;
             }
         }
     }
-}
 
-// Appel de la fonction au chargement de la page
-masquerContenu();
+}
+// Call the function when the page loads
+hideContent();
